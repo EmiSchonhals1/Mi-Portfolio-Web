557ffc3bebeaf19fbd2141fa31d540d668aeb922
EmiSchonhals1 <schonhalsemiliano@gmail.com>
2024-01-02 11:27:54 -0300

    update8

--- asgi.py
+++ asgi.py
@@ -1,6 +1,8 @@
 import os
 from django.core.asgi import get_asgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio.settings')
 
 application = get_asgi_application()
+application = WhiteNoise(application)
