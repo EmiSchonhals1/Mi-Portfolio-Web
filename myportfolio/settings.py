b5274bfa392e00c6b04144ad9fbf5200299a8893
EmiSchonhals1 <schonhalsemiliano@gmail.com>
2024-01-10 11:50:57 -0300

    Change site title to EmiSchonhals

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', '127.0.0.1')]
+ALLOWED_HOSTS = ['*']
 
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
